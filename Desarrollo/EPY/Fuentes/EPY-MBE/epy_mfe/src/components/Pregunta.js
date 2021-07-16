13317ceedd9261c0bf93ad813586a24ab79cbf18
DarkBlood202 <32721945+DarkBlood202@users.noreply.github.com>
2021-07-16 17:22:10 -0500

    consumo de api preguntas: fetch desde la base de datos

--- Desarrollo/EPY/Fuentes/EPY-MBE/epy_mfe/src/components/Pregunta.js
+++ Desarrollo/EPY/Fuentes/EPY-MBE/epy_mfe/src/components/Pregunta.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 
 export class Pregunta extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+        const { pregunta } = this.props;
         return (
             <li className="cards__item">
                 <div className="cardPregunta ">
                     <div className="card__content">
-                        <div className="card__title">PREGUNTA</div>
-                        <p className="card__text">Esta es una breve descripcion de tu conjetura y plantear la pregunta de manera adecuada dando el contexto necesario para que la pregunta encuentre sentido. </p>
+                        <div className="card__title">{pregunta.titulo}</div>
+                        <p className="card__text">{pregunta.descripcion}</p>
+                        <p className="card__text">Publicado en {pregunta.fecha} por {pregunta.autor}</p>
                         <button className="btnPregunta btn--block card__btn">Marcar Pregunta</button>
                     </div>
                 </div>
