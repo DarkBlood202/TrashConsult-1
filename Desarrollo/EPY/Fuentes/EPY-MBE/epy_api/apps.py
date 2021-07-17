5e6f45b6546b33fa8f9ff1eb47f51b9379c4e35d
DarkBlood202 <32721945+DarkBlood202@users.noreply.github.com>
2021-07-16 19:03:43 -0500

    eliminacion de archivos residuales

--- Desarrollo/EPY/Fuentes/EPY-MBE/epy_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EpyApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'epy_api'
--- Desarrollo/EPY/Fuentes/EPY-MBE/epy_mfe/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EpyMfeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'epy_mfe'
